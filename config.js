649a1bdea4735a9dd24f8b82574c3048dc4d9ce3
Sivakumar Rajendran <siva.rajendran@getdropstream.com>
2024-02-11 19:04:21 +0530

    removed keys

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const dynamoose = require("dynamoose");
 
 const initDynamoDB = () => {
   dynamoose.AWS.config.update({
-    accessKeyId: "AKIARPY37TSO6K6MNZI4",
-    secretAccessKey: "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",
+    accessKeyId: "",
+    secretAccessKey: "",
     region: "ap-south-1",
   });
 };
